628b4ba784349a40059ddbb15c4e92b667db5df3
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-10-15 00:55:53 +0300

    fix: edit styles

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,23 +1,26 @@
-import { useAppDispatch } from "../../services/hooks";
+import { useAppDispatch, useAppSelector } from "../../services/hooks";
+import { getAllTasks } from "../../services/task-list/task-list-selectors";
 import { removeAllTasks } from "../../services/task-list/task-list-slice";
 import styles from "./app-header.module.scss";
 import FilterOptions from "./filter/filter-options";
 
 const AppHeader = () => {
   const dispatch = useAppDispatch();
+  const allTasks = useAppSelector(getAllTasks);
   function handleRemoveButtonClick() {
     dispatch(removeAllTasks());
   }
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Список задач</h1>
+      <h1 className={styles.title}>Список&nbsp;задач</h1>
       <FilterOptions />
       <button
         className={styles.remove_button}
         type="button"
         onClick={handleRemoveButtonClick}
+        disabled={!allTasks.length}
       >
-        Удалить все
+        Удалить&nbsp;все
       </button>
     </header>
   );
